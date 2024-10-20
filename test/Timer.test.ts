38457c93de7a0e1a5be4ad3d0918dfb426ce041e
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2024-10-21 07:44:44 +0900

    update test for Timer.ts

--- test/Timer.test.ts
+++ test/Timer.test.ts
@@ -1,45 +1,70 @@
 import Timer from "../src/ts/Timer";
 
+// disable to save/load local strage
+class Timer2 extends Timer {
+    setStorage() {}
+    getStorage() {
+        return {};
+    }
+    clearStorage() {}
+}
+
 
 jest.useFakeTimers();
 
-test("Check Timer class", () => {
-    // disable to save/load local strage
-    class Timer2 extends Timer {
-        setStorage() {}
-        getStorage() {
-            return {};
-        }
-        clearStorage() {}
-    }
+describe("Check Timer class", () => {
+    it("Check isRunning function", () => {
+        let timer = new Timer2();
 
-    let timer = new Timer2();
-    jest.runAllTimers()
+        // befor start
+        timer.clickNewExp();
+        expect(timer.isRunning).toEqual(false);
 
-    // befor start
-    timer.clickNewExp();
-    expect(timer.isRunning).toEqual(false);
+        // start
+        timer.clickStart();
+        expect(timer.isRunning).toEqual(true);
 
-    // start
-    timer.clickStart();
-    expect(timer.isRunning).toEqual(true);
+        // stop
+        timer.clickStart()
+        expect(timer.isRunning).toEqual(false);
+    });
 
-    // pass 3 sec
-    jest.advanceTimersByTime(3000);
-    expect(timer.getTime(1)).toEqual(3000);
+    it("Check sliderChanged function", () => {
+        let timer = new Timer2();
+        jest.runAllTimers()
 
-    // change speed
-    // pass 3 sec (5x)
-    timer.sliderChanged();
-    jest.advanceTimersByTime(3000);
-    expect(timer.getTime(5)).toEqual(18000);
+        // befor start
+        timer.clickNewExp();
 
-    // pass 1 sec (10x)
-    timer.sliderChanged();
-    jest.advanceTimersByTime(1000);
-    expect(timer.getTime(10)).toEqual(28000);
+        // start
+        timer.clickStart();
 
-    // stop
-    timer.clickStart()
-    expect(timer.isRunning).toEqual(false);
+        // pass 3 sec
+        //  => 3000 (msec)
+        jest.advanceTimersByTime(3000);
+        expect(timer.getTime(1)).toEqual(3000);
+
+        // change speed
+        // pass 3 sec (5x)
+        //  => 3000 + 3000 x 5 = 18000
+        timer.sliderChanged();
+        jest.advanceTimersByTime(3000);
+        expect(timer.getTime(5)).toEqual(18000);
+
+        // pass 1 sec (10x)
+        //  => 18000 + 1000 x 10 = 28000
+        timer.sliderChanged();
+        jest.advanceTimersByTime(1000);
+        expect(timer.getTime(10)).toEqual(28000);
+
+        // pass 2 sec (1x)
+        //  => 28000 + 2000 = 30000
+        timer.sliderChanged();
+        jest.advanceTimersByTime(2000);
+        expect(timer.getTime(1)).toEqual(30000);
+
+        // stop
+        timer.clickStart()
+    });
 });
+
