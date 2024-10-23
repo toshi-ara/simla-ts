3a24684f10572877205868554a25bcab1de21c2e
ARA Toshiaki <toshiaki.ara@gmail.com>
2024-10-23 20:13:43 +0900

    v2.1.0

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'cache-simla-v2.0.0';
+const CACHE_NAME = 'cache-simla-v2.1.0';
 const urlsToCache = [
     'sim_local_anesthetics.html',
     './dist/sim_local_anesthetics.js'
