ae6fcd2999cf7a04e2707fea3aa131e42bb63aaf
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2024-10-23 18:05:55 +0900

    delete unused variable

--- src/ts/ConstVal.ts
+++ src/ts/ConstVal.ts
@@ -3,7 +3,6 @@ export const MU0 = [[75, 8], [67, 5], [43, 6], [30, 10]];
 export const LOG_SIGMA0 = [[2.2, 0.4], [2.4, 0.4], [2.4, 0.4], [2.5, 0.5]];
 export const ADR = 0.7;
 export const MU0_adj = 0;
-export const D_MU0 = [0, 4];
 
 
 ////////////////////////////////////////
